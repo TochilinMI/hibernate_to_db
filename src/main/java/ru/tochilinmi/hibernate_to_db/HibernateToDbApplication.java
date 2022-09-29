0ba05fed36a601cbab7ebccc467bbfb5ee9b88eb
TochilinMI <tochilin.m.i@mail.ru>
2022-09-29 22:07:20 +0300

    Reed employees from db

--- src/main/java/ru/tochilinmi/hibernate_to_db/HibernateToDbApplication.java
+++ src/main/java/ru/tochilinmi/hibernate_to_db/HibernateToDbApplication.java
@@ -1,10 +1,13 @@
 package ru.tochilinmi.hibernate_to_db;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.tochilinmi.hibernate_to_db.entities.EmployeeEntity;
 import ru.tochilinmi.hibernate_to_db.utils.HibernateUtil;
+
+import java.util.List;
 
 @SpringBootApplication
 public class HibernateToDbApplication {
@@ -12,6 +15,7 @@ public class HibernateToDbApplication {
     public static void main(String[] args) {
         SpringApplication.run(HibernateToDbApplication.class, args);
 
+//        set employees
         try(Session session = HibernateUtil.getSession()) {
             session.beginTransaction();
 
@@ -24,6 +28,26 @@ public class HibernateToDbApplication {
             session.getTransaction().commit();
         }catch (Throwable cause){
             cause.printStackTrace();
+        }
+        
+//        reed from db
+        List<EmployeeEntity> list = null;
+
+        try(Session session = HibernateUtil.getSession()) {
+            session.beginTransaction();
+
+            Query query = session.createQuery("FROM EmployeeEntity");
+            list = (List<EmployeeEntity>) query.list();
+
+            session.getTransaction().commit();
+        }catch (Throwable cause){
+            cause.printStackTrace();
+        }
+
+        if(list != null && !list.isEmpty()){
+            for (EmployeeEntity employee : list){
+                System.out.println(employee);
+            }
         }
 
     }
