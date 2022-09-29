f817461fdc54bbb4ed99068de00e4e1ff011d384
TochilinMI <tochilin.m.i@mail.ru>
2022-09-29 21:51:34 +0300

    Init commit

---
+++ src/main/java/ru/tochilinmi/hibernate_to_db/HibernateToDbApplication.java
@@ -0,0 +1,13 @@
+package ru.tochilinmi.hibernate_to_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateToDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateToDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/tochilinmi/hibernate_to_db/HibernateToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tochilinmi.hibernate_to_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateToDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
